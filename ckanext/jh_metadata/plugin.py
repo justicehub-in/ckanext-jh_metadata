9405624991c98b473519fdcb12a3c9449c344ad3
curioswati <swati@civicdatalab.in>
2020-02-05 17:49:58 +0530

    update plugin to add custom metadata field with validation
    - this is a fallback plugin which handles validation for all datasets
    which don't have any other plugin attached for validation

--- ckanext/jh_metadata/plugin.py
+++ ckanext/jh_metadata/plugin.py
@@ -1,13 +1,60 @@
-import ckan.plugins as plugins
-import ckan.plugins.toolkit as toolkit
+import ckan.plugins as p
+import ckan.plugins.toolkit as tk
 
 
-class Jh_MetadataPlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IConfigurer)
+class Jh_MetadataPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
 
-    # IConfigurer
+    # with IDatasetForm plugin interface,
+    # a CKAN plugin can add custom, first-class metadata fields to CKAN datasets,
+    # and can do custom validation of these fields.
+    p.implements(p.IDatasetForm)
+
+    # This interface allows to implement a function update_config()
+    # that allows us to update the CKAN config,
+    # in our case we want to add an additional location for CKAN to look for templates.
+    p.implements(p.IConfigurer)
 
     def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic', 'jh_metadata')
+        # Add this plugin's templates dir to CKAN's extra_template_paths, so
+        # that CKAN will use this plugin's custom templates.
+        tk.add_template_directory(config_, 'templates')
+
+        tk.add_public_directory(config_, 'public')
+        tk.add_resource('fanstatic', 'jh_metadata')
+
+    def _modify_package_schema(self, schema):
+        schema.update({
+            'custom_text': [tk.get_validator('ignore_missing'),
+                            tk.get_converter('convert_to_extras')]
+        })
+        return schema
+
+    def create_package_schema(self):
+        # let's grab the default schema in our plugin
+        schema = super(Jh_MetadataPlugin, self).create_package_schema()
+        schema = self._modify_package_schema(schema)
+        return schema
+
+    def update_package_schema(self):
+        schema = super(Jh_MetadataPlugin, self).update_package_schema()
+        schema = self._modify_package_schema(schema)
+        return schema
+
+    def show_package_schema(self):
+        schema = super(Jh_MetadataPlugin, self).show_package_schema()
+
+        schema.update({
+            'custom_text': [tk.get_converter('convert_from_extras'),
+                            tk.get_validator('ignore_missing')]
+        })
+        return schema
+
+    def is_fallback(self):
+        # Return True to register this plugin as the default handler for
+        # package types not handled by any other IDatasetForm plugin.
+        return True
+
+    def package_types(self):
+        # This plugin doesn't handle any special package types, it just
+        # registers itself as the default (above).
+        return []
